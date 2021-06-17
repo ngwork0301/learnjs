265bf7188c8bf308f34dbb42447f97ce5f6ebb33
NGW <tnkngw29@gmail.com>
2021-06-18 06:41:26 +0900

    3.3 Add form and a click action.

--- public/tests/app_spec.js
+++ public/tests/app_spec.js
@@ -42,4 +42,20 @@ describe('LearnJS', function () {
             expect(view.find('[data-name="code"]').text()).toEqual('function problem() { return __; }');
         });
     });
+    describe('anser section', function () {
+        var view;
+        beforeEach(function() {
+            view = learnjs.problemView('1');
+        });
+        it('ボタンアクションで正しい答えをチェックできる (can check a correct answer by hitting a button)', function () {
+            view.find('.answer').val('true');
+            view.find('.check-btn').click();
+            expect(view.find('.result').text()).toEqual('Correct!');
+        });
+        it('不正解をリジェクトする (reject an incorrect answer)', function(){
+            view.find('.answer').val('false');
+            view.find('.check-btn').click();
+            expect(view.find('.result').text()).toEqual('Incorrect!')
+        });
+    })
 });
